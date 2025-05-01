490975957bf40e8952313c0f2711075e81fd55e6
luwbe1 <luwbe1@naver.com>
2025-05-02 02:05:54 +0900

    Chore: 배포 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-3/' : '/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
