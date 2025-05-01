772a98a60d7e985439c87cfd5917e9a9b8c2e61f
luwbe1 <luwbe1@naver.com>
2025-05-02 00:38:24 +0900

    Refactor: App.tsx 수정, index.tsx 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App.tsx';
+import App from './app/App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </StrictMode>
 );
