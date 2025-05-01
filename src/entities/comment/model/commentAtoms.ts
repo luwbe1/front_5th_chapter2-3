f6e2237eaee3266344250814d7985ce0193adcdd
luwbe1 <luwbe1@naver.com>
2025-05-01 19:12:48 +0900

    Refactor: atom 파일 이름 변경

--- src/entities/comment/model/commentAtoms.ts
+++ src/entities/comment/model/commentAtoms.ts
@@ -3,12 +3,10 @@ import { Comment } from './type';
 
 export const commentsAtom = atom<Record<number, Comment[]>>({});
 export const selectedCommentAtom = atom<Comment | null>(null);
-
 export const newCommentAtom = atom({
   body: '',
   postId: 0,
   userId: 1,
 });
-
 export const showAddCommentDialogAtom = atom(false);
 export const showEditCommentDialogAtom = atom(false);
--- src/entities/post/model/postAtoms.ts
+++ src/entities/post/model/postAtoms.ts
@@ -3,18 +3,16 @@ import { Post, Tag } from './type';
 
 export const postsAtom = atom<Post[]>([]);
 export const totalAtom = atom(0);
-
 export const newPostAtom = atom({
   title: '',
   body: '',
   userId: 1,
 });
+export const tagsAtom = atom<Tag[]>([]);
 
-export const showAddDialogAtom = atom(false);
+export const loadingAtom = atom(false);
 
 export const selectedPostAtom = atom<Post | null>(null);
+export const showAddDialogAtom = atom(false);
 export const showEditDialogAtom = atom(false);
-
 export const showPostDetailDialogAtom = atom(false);
-
-export const tagsAtom = atom<Tag[]>([]);
